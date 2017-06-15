0beb562767bd37a0ebcc3124d05deb5a03654e6e
Nurken Ainakulov <Nurken Ainakulov>
2017-06-15 10:58:12 +0600

    ChangeOrbits Moving form orbit to orbit

---
+++ Assets/Orbits.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbits : MonoBehaviour {
+    
+    
+    public List<GameObject> orbits = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        initOrbits();
+    }
+
+    void initOrbits()
+    {
+        try
+        {
+            int numberofChilds = transform.childCount;
+            for (int i = 0; i < numberofChilds; i++)
+            {
+                orbits.Add(transform.GetChild(i).gameObject);
+            }
+        }
+        catch
+        {
+            Debug.Log("Orbits asiignment error");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,45 +6,89 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    //Player's start and end point
+    [Header("Transsdorms")]
     public Transform endOrbitTarget;
     public Transform startOrbitTarget;
 
+    [SerializeField]
+    private int orbitIndex;
+    public Orbits orbit;
+
+    //Speed of player toward the end point
     public float speed = 0.1f;
-
-    private Vector3 target;
-
-
-
+    
     void Start()
     {
         transform.position = startOrbitTarget.position;
+        orbitIndex = 0;
+        startOrbitTarget = orbit.orbits[0].transform.Find("start").transform;
+        endOrbitTarget = orbit.orbits[0].transform.Find("end").transform;
     }
 
 
     void Update()
     {
-        
-        SatMovement();
-
-
+        LookAt();
+        Move();
     }
 
-    void SatMovement()
+    /*** rotate the object toward the mouse ***/
+    void LookAt()
     {
         try
-        {
-            //rotate the object toward the mouse
+        {   
             Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 directionLook = (mouseScreenPosition - (Vector2)transform.position).normalized;
             transform.up = directionLook;
-
-            //move to target
-            transform.position = Vector3.MoveTowards(transform.position, endOrbitTarget.position, speed * Time.deltaTime);
         }
         catch 
         {
-            Debug.Log("Game Object Physics error (Player movement ) ");
+            Debug.Log("Player Controller look at mouse error ");
         }
         
     }
+
+    /*** move to target ***/
+    void Move()
+    {
+        try
+        {
+           
+            transform.position = Vector3.MoveTowards(transform.position, endOrbitTarget.position, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, endOrbitTarget.position) <= 0)
+            {
+                changeOrbit();
+            }
+        }
+        catch
+        {
+            Debug.Log("Player Controller Move (by orbit) error ");
+        }
+
+    }
+
+    /*** changeOrbit ***/
+    void changeOrbit()
+    {
+        try
+        {
+            orbitIndex++;
+            if (orbitIndex > orbit.orbits.Count)
+            {
+                // Game Over
+                orbitIndex = 0;
+            }
+
+            startOrbitTarget = orbit.orbits[orbitIndex].transform.Find("start").transform;
+            endOrbitTarget = orbit.orbits[orbitIndex].transform.Find("end").transform;
+            transform.position = startOrbitTarget.position;
+        }
+        catch
+        {
+            Debug.Log("Player Controller Move (by orbit) error ");
+        }
+
+    }
+
 }
